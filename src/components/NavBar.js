974cb32ae70358b53f4982e8826d22b631540506
NSugam <neupane.sugam.19.sn@gmail.com>
2024-02-09 18:57:15 +0530

    Update

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -16,7 +16,7 @@ export default function NavBar(props) {
         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
 
           <li className="nav-item">
-            <a className="nav-link" aria-current="page" href="/">Portfolio</a>
+            <a className="nav-link" aria-current="page" href="https://neupanesugam.com.np/">Portfolio</a>
           </li>
 
 
